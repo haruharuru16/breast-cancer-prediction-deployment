9547148879796d1ffdac13ff70b2d82dbe641fa2
Ita Sulistiani <itasulis16@gmail.com>
2021-12-04 13:50:15 +0700

    testing features

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pickle
+import pandas as pd
+import numpy as np
 import streamlit as st
+
+st.set_page_config(layout="wide")
 
 # load the trained model
 pickle_in = open('rf_bsm_final_model.pkl', 'rb')
@@ -11,16 +15,59 @@ bsm_scaler = pickle.load(scaler_in)
 
 
 @st.cache()
+# convert data into dataframe
+def to_dataframe(clump_thickness, uniformity_of_cell_size, uniformity_of_cell_shape, marginal_adhesion, single_epithelial_cell_size, bare_nuclei, bland_chromatin, normal_nucleoli, mitoses):
+    # convert to pandas dataframe
+    data = {'clump_thickness': [clump_thickness],
+            'uniformity_of_cell_size': [uniformity_of_cell_size],
+            'uniformity_of_cell_shape': [uniformity_of_cell_shape],
+            'marginal_adhesion': [marginal_adhesion],
+            'single_epithelial_cell_size': [single_epithelial_cell_size],
+            'bare_nuclei': [bare_nuclei],
+            'bland_chromatin': [bland_chromatin],
+            'normal_nucleoli': [normal_nucleoli],
+            'mitoses': [mitoses]}
+
+    cancer_data = pd.DataFrame(data=data)
+    return cancer_data
+
+
+# data preprocessing
+def data_preprocessing(data):
+    data_prep = data.copy()
+
+    # log transform marginal adhesion and single epithelial cell size data
+    data_prep['marginal_adhesion'] = np.log(data_prep['marginal_adhesion'])
+    data_prep['single_epithelial_cell_size'] = np.log(
+        data_prep['single_epithelial_cell_size'])
+
+    # normalize the data using StandardScaler()
+    data_norm = data_prep.copy()
+    columns = ['clump_thickness', 'uniformity_of_cell_size',
+               'uniformity_of_cell_shape', 'marginal_adhesion',
+               'single_epithelial_cell_size', 'bare_nuclei',
+               'bland_chromatin', 'normal_nucleoli', 'mitoses']
+
+    bsm_scaled_feature = bsm_scaler.transform(data_norm)
+
+    # put the normalized data into dataframe
+    data_balanced = pd.DataFrame(bsm_scaled_feature, columns=columns)
+
+    return data_balanced
+
+
 # defining the function which will make the prediction using the data which the user inputs
-def prediction(clump_thickness, uniformity_of_cell_size, uniformity_of_cell_shape, marginal_adhesion, single_epithelial_cell_size, bare_nuclei, bland_chromatin, normal_nucleoli, mitoses):
+def get_prediction(data_prep):
 
     # make the prediction
-    prediction = classifier.predict([[clump_thickness, uniformity_of_cell_size,
-                                    uniformity_of_cell_shape, marginal_adhesion,
-                                    single_epithelial_cell_size, bare_nuclei,
-                                    bland_chromatin, normal_nucleoli, mitoses]])
+    # prediction = classifier.predict([[clump_thickness, uniformity_of_cell_size,
+    #                                 uniformity_of_cell_shape, marginal_adhesion,
+    #                                 single_epithelial_cell_size, bare_nuclei,
+    #                                 bland_chromatin, normal_nucleoli, mitoses]])
 
-    if prediction == 0:
+    pred_result = classifier.predict(data_prep)
+
+    if pred_result == 0:
         result = 'Benign'
     else:
         result = 'Malignant'
@@ -31,30 +78,55 @@ def prediction(clump_thickness, uniformity_of_cell_size, uniformity_of_cell_shap
 
 
 def main():
-    st.title('Prediksi Kanker Payudara Menggunakan Algoritma Random Forest')
+    st.title('Breast Cancer Prediction Using Random Forest Algorithm')
 
     # input the data
-    clump_thickness = st.sidebar.number_input('Clump Thickness')
-    uniformity_of_cell_size = st.sidebar.number_input(
-        'Uniformity of Cell Size')
-    uniformity_of_cell_shape = st.sidebar.number_input(
-        'Uniformity of Cell Shape')
-    marginal_adhesion = st.sidebar.number_input('Marginal Adhesion')
-    single_epithelial_cell_size = st.sidebar.number_input(
-        'Single Epithelial Cell Size')
-    bare_nuclei = st.sidebar.number_input('Bare Nuclei')
-    bland_chromatin = st.sidebar.number_input('Bland Chromatin')
-    normal_nucleoli = st.sidebar.number_input('Normal Nucleoli')
-    mitoses = st.sidebar.number_input('Mitoses')
+    col1, col2, col3 = st.columns([1, 1, 1])
+    with col1:
+        clump_thickness = st.number_input('Clump Thickness')
+        uniformity_of_cell_size = st.number_input('Uniformity of Cell Size')
+        uniformity_of_cell_shape = st.number_input('Uniformity of Cell Shape')
+
+    with col2:
+        marginal_adhesion = st.number_input('Marginal Adhesion')
+        single_epithelial_cell_size = st.number_input(
+            'Single Epithelial Cell Size')
+        bare_nuclei = st.number_input('Bare Nuclei')
+
+    with col3:
+        bland_chromatin = st.number_input('Bland Chromatin')
+        normal_nucleoli = st.number_input('Normal Nucleoli')
+        mitoses = st.number_input('Mitoses')
 
     # when the 'Predict' is clicked, make the prediction and store it
-    if st.sidebar.button('Predict'):
-        result = prediction(clump_thickness, uniformity_of_cell_size,
-                            uniformity_of_cell_shape, marginal_adhesion,
-                            single_epithelial_cell_size, bare_nuclei,
-                            bland_chromatin, normal_nucleoli, mitoses)
-        st.success(f'Prediction Result = {result}')
-        st.write('Will be filled by the data')
+    if st.button('Predict'):
+        # convert data into dataframe
+        cancer_data = to_dataframe(clump_thickness, uniformity_of_cell_size,
+                                   uniformity_of_cell_shape, marginal_adhesion,
+                                   single_epithelial_cell_size, bare_nuclei,
+                                   bland_chromatin, normal_nucleoli, mitoses)
+        # st.write(result)
+        # st.write(cancer_data)
+
+        data_prep = data_preprocessing(cancer_data)
+        # st.write(data_prep)
+        swap_data = cancer_data.T
+
+        result = get_prediction(data_prep)
+        # st.write(swap_data)
+        # st.write(result)
+
+        summary, predicted = st.columns([1, 2])
+        with summary:
+            st.markdown('**Prediction Summary**')
+            swap_data.reset_index(inplace=True)
+            swap_data = swap_data.rename(
+                {'index': 'features', 0: 'data_input'}, axis=1)
+            st.write(swap_data)
+
+        with predicted:
+            st.markdown('**Predicted Result**')
+            st.write(f'Predicted Cancer Type : **{result}**')
 
 
 if __name__ == '__main__':
