0c40e721687547d8af18e420876663fba0fe1b3b
Ita Sulistiani <itasulis16@gmail.com>
2021-12-05 15:19:47 +0700

    final commit (maybe)

--- helper.py
+++ helper.py
@@ -1,1 +1,63 @@
-# copy fungsi selain main sama if name kesini ea
+import pickle
+import pandas as pd
+import numpy as np
+
+
+def load_model(model_name):  # function for load the model and scaler
+    model_in = open(model_name, 'rb')
+    model = pickle.load(model_in)
+
+    return model
+
+
+def to_dataframe(clump_thickness, uniformity_of_cell_size, uniformity_of_cell_shape, marginal_adhesion, single_epithelial_cell_size, bare_nuclei, bland_chromatin, normal_nucleoli, mitoses):
+    # convert data input into a dataframe
+    # convert to pandas dataframe
+    data = {'clump_thickness': [clump_thickness],
+            'uniformity_of_cell_size': [uniformity_of_cell_size],
+            'uniformity_of_cell_shape': [uniformity_of_cell_shape],
+            'marginal_adhesion': [marginal_adhesion],
+            'single_epithelial_cell_size': [single_epithelial_cell_size],
+            'bare_nuclei': [bare_nuclei],
+            'bland_chromatin': [bland_chromatin],
+            'normal_nucleoli': [normal_nucleoli],
+            'mitoses': [mitoses]}
+
+    cancer_data = pd.DataFrame(data=data)
+    return cancer_data
+
+
+def data_preprocessing(data, bsm_scaler):  # preprocess the data
+    data_prep = data.copy()
+
+    # log transform marginal adhesion and single epithelial cell size data
+    data_prep['marginal_adhesion'] = np.log(data_prep['marginal_adhesion'])
+    data_prep['single_epithelial_cell_size'] = np.log(
+        data_prep['single_epithelial_cell_size'])
+
+    # normalize the data using StandardScaler()
+    data_norm = data_prep.copy()
+    columns = ['clump_thickness', 'uniformity_of_cell_size',
+               'uniformity_of_cell_shape', 'marginal_adhesion',
+               'single_epithelial_cell_size', 'bare_nuclei',
+               'bland_chromatin', 'normal_nucleoli', 'mitoses']
+
+    bsm_scaled_feature = bsm_scaler.transform(data_norm)
+
+    # put the normalized data into dataframe
+    data_balanced = pd.DataFrame(bsm_scaled_feature, columns=columns)
+
+    return data_balanced
+
+
+# defining the function which will make the prediction using the data which the user inputs
+def get_prediction(data_prep, classifier):
+    # make the prediction
+    pred_result = classifier.predict(data_prep)
+
+    if pred_result == 0:
+        result = 'Benign'
+    else:
+        result = 'Malignant'
+
+    return result
--- main.py
+++ main.py
@@ -1,84 +1,17 @@
-import pickle
-import pandas as pd
-import numpy as np
 import streamlit as st
+from helper import load_model, to_dataframe, data_preprocessing, get_prediction
 
 st.set_page_config(layout="wide")
 
-# load the trained model
-pickle_in = open('rf_bsm_final_model.pkl', 'rb')
-classifier = pickle.load(pickle_in)
 
-# load the scaler
-scaler_in = open('bsm_scaler.pkl', 'rb')
-bsm_scaler = pickle.load(scaler_in)
-
-
-@st.cache()
-# convert data into dataframe
-def to_dataframe(clump_thickness, uniformity_of_cell_size, uniformity_of_cell_shape, marginal_adhesion, single_epithelial_cell_size, bare_nuclei, bland_chromatin, normal_nucleoli, mitoses):
-    # convert to pandas dataframe
-    data = {'clump_thickness': [clump_thickness],
-            'uniformity_of_cell_size': [uniformity_of_cell_size],
-            'uniformity_of_cell_shape': [uniformity_of_cell_shape],
-            'marginal_adhesion': [marginal_adhesion],
-            'single_epithelial_cell_size': [single_epithelial_cell_size],
-            'bare_nuclei': [bare_nuclei],
-            'bland_chromatin': [bland_chromatin],
-            'normal_nucleoli': [normal_nucleoli],
-            'mitoses': [mitoses]}
-
-    cancer_data = pd.DataFrame(data=data)
-    return cancer_data
-
-
-# data preprocessing
-def data_preprocessing(data):
-    data_prep = data.copy()
-
-    # log transform marginal adhesion and single epithelial cell size data
-    data_prep['marginal_adhesion'] = np.log(data_prep['marginal_adhesion'])
-    data_prep['single_epithelial_cell_size'] = np.log(
-        data_prep['single_epithelial_cell_size'])
-
-    # normalize the data using StandardScaler()
-    data_norm = data_prep.copy()
-    columns = ['clump_thickness', 'uniformity_of_cell_size',
-               'uniformity_of_cell_shape', 'marginal_adhesion',
-               'single_epithelial_cell_size', 'bare_nuclei',
-               'bland_chromatin', 'normal_nucleoli', 'mitoses']
-
-    bsm_scaled_feature = bsm_scaler.transform(data_norm)
-
-    # put the normalized data into dataframe
-    data_balanced = pd.DataFrame(bsm_scaled_feature, columns=columns)
-
-    return data_balanced
-
-
-# defining the function which will make the prediction using the data which the user inputs
-def get_prediction(data_prep):
-
-    # make the prediction
-    # prediction = classifier.predict([[clump_thickness, uniformity_of_cell_size,
-    #                                 uniformity_of_cell_shape, marginal_adhesion,
-    #                                 single_epithelial_cell_size, bare_nuclei,
-    #                                 bland_chromatin, normal_nucleoli, mitoses]])
-
-    pred_result = classifier.predict(data_prep)
-
-    if pred_result == 0:
-        result = 'Benign'
-    else:
-        result = 'Malignant'
-
-    return result
-
-# main function
-
-
-def main():
+def main():  # main function
     st.title('Breast Cancer Prediction Using Random Forest Algorithm')
+
+    # load the trained model
+    classifier = load_model('rf_bsm_final_model.pkl')
+
+    # load the scaler
+    bsm_scaler = load_model('bsm_scaler.pkl')
 
     # input the data
     col1, col2, col3 = st.columns([1, 1, 1])
@@ -105,17 +38,17 @@ def main():
                                    uniformity_of_cell_shape, marginal_adhesion,
                                    single_epithelial_cell_size, bare_nuclei,
                                    bland_chromatin, normal_nucleoli, mitoses)
-        # st.write(result)
-        # st.write(cancer_data)
 
-        data_prep = data_preprocessing(cancer_data)
-        # st.write(data_prep)
+        # preprocess the data
+        data_prep = data_preprocessing(cancer_data, bsm_scaler)
+
+        # get the data summary
         swap_data = cancer_data.T
 
-        result = get_prediction(data_prep)
-        # st.write(swap_data)
-        # st.write(result)
+        # get prediction result
+        result = get_prediction(data_prep, classifier)
 
+        # show the data summary and the predicted result
         summary, predicted = st.columns([1, 2])
         with summary:
             st.markdown('**Prediction Summary**')
